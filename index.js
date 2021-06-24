4ab3a2cab32076a7fdd148b00bfdd0f3b7b9aa39
Procoder16 <smsoumik1608@gmail.com>
2021-06-24 18:44:34 +0530

    taking js code to a different file altogether

---
+++ index.js
@@ -0,0 +1,6 @@
+document.write("This is writing on the document");
+//Javascript console API
+console.log("Hello World!!!");
+alert("Hey there, what's up?");
+console.warn("This is a warning!");
+console.error("This is an error");
